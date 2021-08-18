052e320c0e56b085ac876737ac7715728dfa534e
serg1557733 <serg1557733@ukr.net>
2021-08-18 16:43:17 +0300

    form sign in

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
 const olerlay = document.querySelector('.menu__overlay');
-const logIn = document.querySelector('.header__link');
+
 
 hamburger.addEventListener('click',() => {
     menu.classList.add('active')
@@ -16,7 +16,17 @@ closeElem.addEventListener('click',() => {
     hamburger.classList.remove('hamburger_active')
     olerlay.classList.remove('menu__overlay_active')
 });
+//log in  
+const logIn = document.querySelector('.login');
+const formLogin = document.querySelector('.form__login');
+const closeElemLog = document.querySelector('.menu__close_login');
 
 logIn.addEventListener('click', ()=>{
+    formLogin.classList.add('form__login_active')
     olerlay.classList.add('menu__overlay_active')
 });
+closeElemLog.addEventListener('click',() => {
+    formLogin.classList.remove('form__login_active')
+    olerlay.classList.remove('menu__overlay_active')
+});
+
